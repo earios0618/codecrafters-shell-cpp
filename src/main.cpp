817ae542cd61728c9f964ab07f1fc494ff1caf94
earios0618 <earios0615@gmail.com>
2026-05-08 16:10:03 -0500

    bwah

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <commands.h>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,14 +8,36 @@ int main() {
   std::cerr << std::unitbuf;
   while (true) {
     std::cout << "$ ";
-    std::string command;
-    std::getline(std::cin, command);
-    if (command == "exit") {
-      break;
-    } else if (command.substr(0, 4) == "echo") {
-      std::cout << command.substr(5) << std::endl;
-    } else {
-      std::cout << command + ": command not found\n";
+    std::string commandLine;
+    std::getline(std::cin, commandLine);
+    Command command = parse_command(commandLine);
+    switch (command) {
+      case CMD_EXIT:
+        break;
+      case CMD_ECHO:
+        std::cout << commandLine.substr(5) << std::endl;
+        break;
+      case CMD_TYPE: {
+        std::string parameter = commandLine.substr(5);
+        Command subCommand = parse_command(parameter);
+        if (subCommand == CMD_INVALID) {
+          std::cout << parameter + ": not found\n";
+        } else {
+          std::cout << parameter + " is a shell builtin\n";
+        }
+      }
+      default:
+        std::cout << commandLine + ": command not found\n";
     }
+  }
+}
+
+Command parse_command(std::string commandLine) {
+  if (commandLine == "exit") {
+    return CMD_EXIT;
+  } else if (commandLine.substr(0, 4) == "echo") {
+    return CMD_ECHO;
+  } else {
+    return CMD_INVALID;
   }
 }
