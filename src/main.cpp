5001b9d53fd7c5b84c68926e3a0ec82623ae9360
earios0618 <earios0615@gmail.com>
2026-05-08 15:30:44 -0500

    bwah

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,6 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
-    std::cout << command + ": command not found";
+    std::cout << command + ": command not found\n";
   }
 }
