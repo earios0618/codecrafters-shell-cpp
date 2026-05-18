2b5775d353f18bb32e5bfd8a97de31d3ee948f48
Eli <earios0615@gmail.com>
2026-05-18 12:50:20 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -232,7 +232,8 @@ char** attemptCompletion(const char* text, int start, int end) {
       args[3] = tokens.size() > 2 ? tokens.at(tokens.size() - 2).c_str() : nullptr;
       args[4] = nullptr;
       setenv("COMP_LINE", rl_line_buffer, 1);
-      setenv("COMP_POINT", std::to_string(rl_point).c_str(), 1);
+      std::string compPointStr = std::to_string(rl_point);
+      setenv("COMP_POINT", compPointStr.c_str(), 1);
       execv(customCompletions[tokens[0]].c_str(), (char* const*) args);
     } else if (pid > 0) {
       waitpid(pid, nullptr, 0);
