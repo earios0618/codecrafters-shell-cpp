7b935ebc24447709160846a586cfeb83745a0b56
Eli <earios0615@gmail.com>
2026-05-28 18:27:06 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -429,10 +429,12 @@ void parse_args(std::string& commandLine, std::vector<std::string>& args) {
       arg.push_back(ch);
       takeLiteral = false;
     } else if (ch == '"') {
+      std::cout << "HAPP\n";
       inDouble = !inDouble;
     } else if (inDouble) {
       arg.push_back(ch);
     } else if (ch == '\'') {
+      std::cout << "HAPP\n";
       inSingle = !inSingle;
     } else if (inSingle) {
       arg.push_back(ch);
